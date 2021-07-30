36a3e04692294346506d30cb7324a1e5549a85b9
ValeriiRo <kosmos.rem@yandex.ru>
2021-07-30 15:22:55 +0300

    Inheritance_fix

--- main.py
+++ main.py
@@ -14,12 +14,6 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-class Lecturer (Mentor):
-    pass
-
-class Reviewer (Mentor):
-    pass
-
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -29,15 +23,22 @@ class Reviewer (Mentor):
         else:
             return 'Ошибка'
 
+class Lecturer (Mentor):
+    pass
+
+
+class Reviewer (Mentor):
+    pass
+
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_lecturer.rate_hw(best_student, 'Python', 10)
+cool_lecturer.rate_hw(best_student, 'Python', 10)
+cool_lecturer.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
