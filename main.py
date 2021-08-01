67e11d4282adf22eccedbdfb1df3b63b6e030fca
ValeriiRo <kosmos.rem@yandex.ru>
2021-08-01 17:45:00 +0300

    removed_the_extra_text

--- main.py
+++ main.py
@@ -157,16 +157,3 @@ for courses in list_courses:
                 sum_ratings += estimation
                 number_ratings += 1
     print(f'средняя оценка всех Лекторов по курсу {courses}: {sum_ratings / number_ratings}')
-
-# best_student = Student('Ruoy', 'Eman', 'your_gender')
-# best_student.courses_in_progress += ['Python']
-#
-# cool_mentor = Mentor('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
-#
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-#
-#
-# print(best_student.grades)
